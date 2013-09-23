4e698a2358e1f01b8f3082dae9e9fd785bbd0b5b
Ramunas <munasgeras@gmail.com>
2013-09-23 11:12:40 +0200

    Pakomentavau pusiaukirtos metoda

--- nlsm/pusiaukirtosmetodas.m
+++ nlsm/pusiaukirtosmetodas.m
@@ -3,8 +3,24 @@ function saknis = pusiaukirtosmetodas(func,x1,x2,filter,tol)
 % IVEDIMO PARAMETRAI:
 % filter = singuliarumo filtras: 0 = iðjungta (pagal nutylejima), 1 = ijungta.
 % tol = paklaida (pagal nutylejima 0.0001).
+
+% nargin grazina ivesties argumenta skaiciu
+%filter filtruoja skaiciu skaitmeniniu butu
+% Tikrina kiek parametru ivesta: 
+% jei ivedama 5, tai vartotojas pasirenka paklaida ir ijungia filtra
+% jei ivedama 4, tai automatiskai nustatoma paklaida
+% jei ivedana 3, tai pradedama isjungtu filtru ir numatyta pakalida
+% truksta apsaugos ribos ivedimui. pvz
 if nargin < 5; tol = 0.0001; end
 if nargin < 4; filter = 0; end
+%if nargin = 4; tol = 0.0001; end
+%if nargin = 3; filter = 0; tol = 0.0001; end
+% if nargin = 2; x2 = x1+100; filter = 0; tol = 0.0001; end
+% if nargin = 1; x1=-100; x2=100; filter = 0; tol = 0.0001; end
+
+% Atliekamas ribos krastu patikrinimas
+% Jei ribos galas tenkina salyga tai is kart grazinama saknis
+% jei ribos vienodu zenklu nera sprendiniu
 f1 = func(x1);
 if f1 == 0.0; saknis = x1; return; end
 f2 = func(x2);
@@ -12,6 +28,7 @@ if f2 == 0.0; saknis = x2; return; end
 if f1*f2 > 0;
 error('Nurodytame intervale (x1,x2) nera funkcijos nulio')
 end
+
 n = ceil(log2(abs(x2 - x1)/tol));
 for i = 1:n
     x3 = 0.5*(x1 + x2);
