867e8fdf2aa2b768dbcd8442f5f93b9a30692654
Ramunas <munasgeras@gmail.com>
2013-09-30 09:21:08 +0200

    Pabaigtas nlsm

--- nlsm/pilnaspusiaukirtosmetodas.m
+++ nlsm/pilnaspusiaukirtosmetodas.m
@@ -1,9 +1,18 @@
 function saknys = pilnaspusiaukirtosmetodas(func,a,b,dx,filter,tol)
 % Pusiaukirtos metodu surandami visi funkcijos f(x) nuliai intervale (a,b).
+% IVEDIMO PARAMETRAI:
+%func - funkcija, kaip m-failas arba anonimine funkcija
+% a,b = pirminio intervalo pradþia ir pabaiga.
+% dx = þingsnio ilgis: (b-a)/n.
+%a<b, intervalas, kuriame ieskosime funkcijos nulio(-iu)
+%PALEIDIMAS: saknys = pilnaspusiaukirtosmetodas(func,a,b,dx,filter,tol)
 saknusk = 0;
 while 1
     [x1,x2] = skaidosmetodas(func,a,b,dx);
+    % jeio buvo NaN priskirtas is karto stabdomas ir grazins NaN(T.y. visas
+    % intervalas buvo perzvelgtas)
     if isnan(x1)
+        disp('Diaugiau intervalu nebera');
         break
     else
         a = x2;
