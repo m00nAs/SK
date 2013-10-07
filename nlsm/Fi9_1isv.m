298b184c4e1eb3fa0c4e57fcf5091184071c6498
Ramunas <munasgeras@gmail.com>
2013-10-07 10:44:38 +0200

    funkciju nuo x isvestines

---
+++ nlsm/Fi9_1isv.m
@@ -0,0 +1,5 @@
+function [ fii ] =Fi9_1isv (x1)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+fii=(-x1)*(2^(-x1 - 1));
+end
---
+++ nlsm/Fi9_2isv.m
@@ -0,0 +1,5 @@
+function [ fi ] =Fi9_2isv (x1)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+fi=-(1/(x1*ln2));
+end
